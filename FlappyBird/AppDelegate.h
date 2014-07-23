2ad78e21bffabb9540434cffd35d92d2a1e13912
enterpi <enterpi@enterpis-Mac-Pro-3.local>
2014-07-23 16:42:30 +0530

    Initial Commit

---
+++ FlappyBird/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlappyBird
+//
+//  Created by enterpi on 7/23/14.
+//  Copyright (c) 2014 enterpi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlappyBird/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlappyBird
+//
+//  Created by enterpi on 7/23/14.
+//  Copyright (c) 2014 enterpi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
